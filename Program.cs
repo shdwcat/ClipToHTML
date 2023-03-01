d09180550f9e9fe9a8dd1c160d1470ac63940974
Quetzy <15136382+shdwcat@users.noreply.github.com>
2023-03-01 13:55:44 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClipToHTML
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (Clipboard.ContainsData(DataFormats.Html))
+            {
+                var cf_html = Clipboard.GetText(TextDataFormat.Html);
+                var html_start = cf_html.IndexOf('<');
+                if (html_start >= 0)
+                {
+                    var html = cf_html.Substring(html_start);
+                    Clipboard.SetText(html);
+                }
+            }
+        }
+    }
+}
